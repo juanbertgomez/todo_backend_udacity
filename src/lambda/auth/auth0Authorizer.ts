89cd0d2065832c444e21d7ed428b7d5a0a4cf9d4
Juan Gomez <juanbertgomez@gmail.com>
2020-04-25 17:23:15 +0200

    add authoriser

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -55,9 +55,23 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // const token = getToken(authHeader)
+  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
+  if(!authHeader) {
+    throw new Error ('No authorization header')
+  }
+
+  if(!authHeader.toLocaleLowerCase().startsWith('bearer ')) {
+    throw new Error('Invalid authorization header')
+  }
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  if(token !== '123') {
+    throw('Invalid token')
+  }
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
