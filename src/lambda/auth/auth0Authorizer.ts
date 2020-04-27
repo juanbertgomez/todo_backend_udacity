610fff28c54a8e77f19e18b564a85dafb16278d5
Juan Gomez <juanbertgomez@gmail.com>
2020-04-27 10:33:45 +0200

    add user id

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -8,12 +8,31 @@ import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-const secret = process.env.AUTH_0_SECRET
+
+let certificate = `-----BEGIN CERTIFICATE-----
+MIIC/zCCAeegAwIBAgIJIZXiCuo/1EG5MA0GCSqGSIb3DQEBCwUAMB0xGzAZBgNV
+BAMTEmp1YW5iZXJ0LmF1dGgwLmNvbTAeFw0yMDA0MjAwODIzNTVaFw0zMzEyMjgw
+ODIzNTVaMB0xGzAZBgNVBAMTEmp1YW5iZXJ0LmF1dGgwLmNvbTCCASIwDQYJKoZI
+hvcNAQEBBQADggEPADCCAQoCggEBAL2b9krEATomaYdVj2PI5+AU+zMkgADcXyZn
+c7sMbaoUEBWDk5IzD5JOFIyzWansSMe1+hWkChwfHlG2Ky17AL1sfVGw/rTKwe7u
+hMEfbefFSzkwz4hdfFwtgQoAd5bgSQnsb9Z4FvYFVdVxedLvmuUFIbaTzRcNG2mJ
+O3pdkIWmd8ca0GeiCclYYsC5pUYgbKpjv71cmA+hNuCLQWQshGnhHv/StJ33WTbw
+XDavnVbsUjTDnidCQCpdAKVRBvApZaTDWk15U93ZefBpjQ/4Ysky4N3oG0HuvOAp
+gxPpkw8rh/JwYIlBIlPlUIYeokaG3Sjp1dVHs6YsrDx5l8Lwp6sCAwEAAaNCMEAw
+DwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUYC/R4NAmPt/It4BHI7QikGneQZUw
+DgYDVR0PAQH/BAQDAgKEMA0GCSqGSIb3DQEBCwUAA4IBAQAh3cJzK7qqMBgj+TdM
+0KNwoH4N1PkYC9XuHpHfEy8YvTCk1vKvIo3aD9Ke7QQEFF1Bawza8fWmEHy8bUKq
+2rpS5+FKCjcMkrDnH5pwe5I5PWgiTnpkGMokJtVc+QdvDI7rdO2ra3Ho6Pc4p7XM
+mOV7b4CEOHC4JyJ9mq6NJjbh47ePRJUl9hmWD/GOFYP6At39dWTnT5dvVJZDzJMl
+tCYE/lLYUUY/w+re72RJNJXgaTN669tO0M8xjI/J1VHB1FJOigibauQMnfjWG7uo
+8JmIiGRXOdeE+lvf4SRXS5qP8tKyd8pT70bS9X/bQ/G9rkJ48Ri2P5Wuim9y+skb
+ZRFG
+-----END CERTIFICATE-----`
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://juanbert.auth0.com/.well-known/jwks.json'
+// const jwksUrl = '...'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -62,15 +81,11 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   //   throw new Error('invalid token')
   // }
 
-  try {
-    const response = await Axios.get(jwksUrl)
-    const certificate = response.data
+    // const response = await Axios.get(jwksUrl)
+    // const certificate = response.data
 
   return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
-  } catch (error) {
-    console.error(error)
-    return undefined
-  }
+
 }
 
 function getToken(authHeader: string): string {
--- src/lambda/http/createTodo.ts
+++ src/lambda/http/createTodo.ts
@@ -3,10 +3,38 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as uuid from 'uuid'
+import * as AWS from 'aws-sdk'
+import { getUserId } from '../utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const itemId = uuid.v4()
+
+  const userId = getUserId(event)
+
+  const newItem = {
+    todoId: itemId, 
+    userId,
+    ...newTodo
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newItem
+  }).promise()
 
   // TODO: Implement creating a new TODO item
-  return undefined
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({newItem})
+  }
 }
