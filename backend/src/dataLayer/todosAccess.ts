df9af430dfb35e8a053dd44aaa4cc264e86bbf85
Juan Gomez <juanbertgomez@gmail.com>
2020-04-30 21:59:38 +0200

    for question review

--- backend/src/dataLayer/todosAccess.ts
+++ backend/src/dataLayer/todosAccess.ts
@@ -3,11 +3,14 @@ import * as AWS  from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 import {TodoItem} from '../models/TodoItem'
 
-// const XAWS = AWSXRay.captureAWS(AWS)
+// import * as uuid from 'uuid'
+
+
+const XAWS = AWSXRay.captureAWS(AWS)
 
 export class TodoAccess {
   constructor (
-    private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+    private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
     private readonly todosTable = process.env.TODOS_TABLE,
     private readonly userIdIndex = process.env.USER_ID_INDEX
 
@@ -29,4 +32,28 @@ export class TodoAccess {
     const items = result.Items
     return items as TodoItem[]
   }
+
+  // async createTodo(userId: string) {
+
+  //   const itemId = uuid.v4()
+
+  //   let createdAt = new Date()
+  //   let done = false
+  //   let attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${itemId}`
+  
+  //   const newItem = {
+  //     todoId: itemId, 
+  //     userId,
+  //     createdAt,
+  //     done,
+  //     attachmentUrl,
+  //     ...newTodo
+  //   }
+  
+  //   await docClient.put({
+  //     TableName: todosTable,
+  //     Item: newItem
+  //   }).promise()
+
+  // }
 }
